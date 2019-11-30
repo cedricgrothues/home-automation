5f7cb6e1b5032b8a5b3e0eef3c7af3ea859ab74b
Cedric Grothues <34948511+cedricgrothues@users.noreply.github.com>
2019-11-30 16:08:55 +0100

    Update service.config
    Finish config service implementation

--- service.config/handler/config.go
+++ service.config/handler/config.go
@@ -4,21 +4,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"time"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 // Config reseambles the parsed config.json file
 type Config struct {
-	Version  string    `json:"version"`
-	Services []Service `json:"services"`
+	path    string
+	Version string `json:"version"`
+	Polling struct {
+		Enabled  bool `json:"enabled"`
+		Interval int  `json:"interval"`
+	} `json:"polling"`
+	Environment string    `json:"environment"`
+	Services    []Service `json:"services"`
 }
 
 // Service reseambles a single home-automation service
 type Service struct {
-	Identifier string `json:"identifier"`
-	Friendly   string `json:"friendly"`
-	Port       int    `json:"port"`
-	// Options    struct{} `json:"options"`
+	Identifier string                 `json:"identifier"`
+	Friendly   string                 `json:"friendly"`
+	Port       int                    `json:"port"`
+	Options    map[string]interface{} `json:"options"`
 }
 
 // Read config files
@@ -26,25 +35,87 @@ func Read(path string) (*Config, error) {
 	b, err := ioutil.ReadFile(path)
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	var c Config
+	c.path = path
 
 	json.Unmarshal(b, &c)
 
-	return nil, nil
+	return &c, nil
+}
+
+// UpdateConfig reads the specified file and updates the config's values accordingly
+func (c *Config) Update() error {
+	b, err := ioutil.ReadFile(c.path)
+
+	if err != nil {
+		return err
+	}
+
+	json.Unmarshal(b, &c)
+
+	return nil
 }
 
 // Watch changes in config files
-func (h *ConfigHandler) Watch(d time.Duration) {
+func (c *Config) Watch(d time.Duration) {
 	for {
-		if err := h.Read(); err != nil {
-			fmt.Printf("Failed to reload %s! Error: %v\n", h.Path, err)
+		if err := c.Update(); err != nil {
+			fmt.Printf("Failed to reload %s! Error: %v\n", c.path, err)
 		}
-
-		fmt.Println(h.Config.Version)
 
 		time.Sleep(d)
 	}
+}
+
+func (c *Config) Get(identifier string) (*Service, error) {
+	for i := range c.Services {
+		if c.Services[i].Identifier == identifier {
+			return &c.Services[i], nil
+		}
+	}
+
+	return nil, fmt.Errorf("Service %s not found in %s", identifier, c.path)
+}
+
+// ReadConfig returns a config for a givend device
+func (c *Config) ReadConfig(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	service, err := c.Get(p[0].Value)
+
+	if err != nil {
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusNotFound)
+
+		w.Write([]byte(fmt.Sprintf("{\"message\":\"%v\"}", err)))
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	bytes, err := json.Marshal(service)
+
+	if err != nil {
+		panic("A problem occured while converting JSON: " + err.Error())
+	}
+
+	w.Write(bytes)
+}
+
+// ReloadConfig reloads the config
+func (c *Config) ReloadConfig(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	err := c.Update()
+
+	if err != nil {
+		panic("could not reload config file")
+	}
+
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte("{\"message\": \"Config file reloaded successfully\"}"))
 }
--- service.config/main.go
+++ service.config/main.go
@@ -2,36 +2,44 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/cedricgrothues/home-automation/libraries/go/errors"
+	"github.com/cedricgrothues/home-automation/libraries/go/readme"
 	"github.com/cedricgrothues/home-automation/service.config/handler"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	// router := httprouter.New()
-	// router.NotFound = http.HandlerFunc(errors.NotFound)
-	// router.MethodNotAllowed = http.HandlerFunc(errors.NotAllowed)
-	// router.PanicHandler = errors.PanicHandler
 
-	// info := readme.Info{Name: "service.config", Friendly: "Config Service"}
-	// router.GET("/", info.Get)
-
-	// router.GET("/devices/:id", routes.GetState)
-	// router.PATCH("/devices/:id", routes.PatchState)
-
-	// errors.Log("service.controller.sonoff", "Failed to start with error:", http.ListenAndServe(":4002", router))
-
-	handler := handler.ConfigHandler{
-		Path: "data/dev.config.json",
-	}
-
-	err := handler.Read()
+	config, err := handler.Read("./data/config.json")
 
 	if err != nil {
 		panic(err)
 	}
 
-	handler.Watch(time.Minute / 4)
+	if config.Polling.Enabled {
+		fmt.Printf("Reloading config every %d milliseconds (%d seconds)\n", config.Polling.Interval, config.Polling.Interval/1000)
+		go config.Watch(time.Millisecond * time.Duration(config.Polling.Interval))
+	}
 
-	fmt.Println(handler.Config.Version)
+	self, err := config.Get("service.config")
+
+	if err != nil {
+		panic("Failed to get own config with error: " + err.Error())
+	}
+
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(errors.NotFound)
+	router.MethodNotAllowed = http.HandlerFunc(errors.NotAllowed)
+	router.PanicHandler = errors.PanicHandler
+
+	info := readme.Info{Name: self.Identifier, Friendly: self.Friendly}
+	router.GET("/", info.Get)
+
+	router.GET("/read/:id", config.ReadConfig)
+	router.GET("/reload", config.ReloadConfig)
+
+	errors.Log("service.config", "Failed to start with error:", http.ListenAndServe(fmt.Sprintf(":%d", self.Port), router))
 }
