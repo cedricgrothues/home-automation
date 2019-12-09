f118ae61ab6ffaf670becf0ffd560a5befcbd0b8
Cedric Grothues <cedricgrothues@gmail.com>
2019-12-09 16:01:53 +0100

    Update discovery.go

--- service.controller.sonos/discovery/discovery.go
+++ service.controller.sonos/discovery/discovery.go
@@ -3,70 +3,89 @@ package discovery
 
 import (
 	"bytes"
-	"fmt"
-	"github.com/cedricgrothues/home-automation/service.controller.sonos/dao"
+	"encoding/xml"
 	"net"
+	"net/http"
 	"strings"
-	"syscall"
 	"time"
 )
 
-// **********************************************
-// TODO: FIGURE OUT WHY WE CANT ESCAPE THIS LOOP
-// **********************************************
+type Sonos struct {
+	Address net.IP `json:"address"`
+	Friendly string `xml:"device>displayName" json:"friendly"`
+}
 
 // Discover looks for new Devices on the local network
-func Discover() ([]*dao.Sonos, error) {
-	search := []string{"M-SEARCH * HTTP/1.1", "HOST: 239.255.255.250:reservedSSDPport", "MAN: ssdp:discover", "MX: 1", "ST: urn:schemas-upnp-org:device:ZonePlayer:1"}
+func Discover(c chan *Sonos, e chan error, d chan bool) {
+	search := []string{"M-SEARCH * HTTP/1.1", "HOST: 239.255.255.250:1900", "MAN: \"ssdp:discover\"", "MX: 1", "ST: urn:schemas-upnp-org:device:ZonePlayer:1"}
+
+	addr, err := net.ResolveUDPAddr("udp", ":1900")
+
+	if err != nil {
+		e <- err
+		return
+	}
+
+	connection, err := net.ListenUDP("udp", addr)
+
+	if err != nil {
+		e <- err
+		return
+	}
+
+	err = connection.SetReadDeadline(time.Now().Add(time.Second * 5))
+
+	if err != nil {
+		e <- err
+		return
+	}
+
+	defer connection.Close()
+
+	multicast, err := net.ResolveUDPAddr("udp", "239.255.255.250:1900")
+
+	if err != nil {
+		e <- err
+		return
+	}
 
 	message := new(bytes.Buffer)
 	message.WriteString(strings.Join(search, "\r\n"))
 
-	socket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+	_, err = connection.WriteTo(message.Bytes(), multicast)
 
 	if err != nil {
-		return nil, err
+		e <- err
+		return
 	}
 
-	err = syscall.SetsockoptInt(socket, syscall.IPPROTO_IP, syscall.IP_MULTICAST_TTL, 2)
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = syscall.Sendto(socket, message.Bytes(), 0, &syscall.SockaddrInet4{
-		Port: 1900,
-		Addr: [4]byte{239, 255, 255, 250},
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	var speakers []*dao.Sonos
-
-	buffer := make([]byte, 2048)
 	for {
-		timeout := syscall.NsecToTimeval(time.Nanosecond.Nanoseconds())
-
-		err := syscall.Select(socket+1, nil, nil, nil, &timeout)
-
-		if err != nil {
+		buf := make([]byte, 1024)
+		_, addr, err := connection.ReadFromUDP(buf)
+		if err, ok := err.(net.Error); ok && err.Timeout() {
+			d <- true
 			break
 		}
 
-		_, addr, err := syscall.Recvfrom(socket, buffer, 0)
+		sonos := Sonos{
+			Address: addr.IP,
+		}
+
+		res, err := http.Get("http://" + sonos.Address.String() + ":1400/xml/device_description.xml")
 
 		if err != nil {
+			e <- err
 			break
 		}
 
-		a := addr.(*syscall.SockaddrInet4)
+		err = xml.NewDecoder(res.Body).Decode(&sonos)
+		res.Body.Close()
 
-		fmt.Println(net.IPv4(a.Addr[0], a.Addr[1], a.Addr[2], a.Addr[3]).String())
+		if err != nil {
+			e <- err
+			break
+		}
 
-		speakers = append(speakers, &dao.Sonos{Address: net.IPv4(a.Addr[0], a.Addr[1], a.Addr[2], a.Addr[3])})
+		c <- &sonos
 	}
-
-	return speakers, nil
 }
