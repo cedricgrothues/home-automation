596ee478229eb6d35a565b7d461307256f8668d5
Cedric Grothues <cedricgrothues@gmail.com>
2019-12-13 21:54:44 +0100

    Update server.go

--- service.api-gateway/server/server.go
+++ service.api-gateway/server/server.go
@@ -1,12 +1,17 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"github.com/cedricgrothues/httprouter"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 
 	"github.com/cedricgrothues/home-automation/service.api-gateway/config"
 )
@@ -37,7 +42,12 @@ func ListenAndServe(c *config.Configuration) error {
 		router.PATCH(prefix+"*service", handler(prefix, p))
 	}
 
-	return http.ListenAndServe(fmt.Sprintf(":%d", c.Port), router)
+	server := &http.Server{Addr: fmt.Sprintf(":%d", c.Port), Handler: router}
+
+	go run(server)
+	graceful(server, time.Second * 5)
+
+	return nil
 }
 
 func handler(prefix string, proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request, httprouter.Params) {
@@ -53,6 +63,27 @@ func handler(prefix string, proxy *httputil.ReverseProxy) func(http.ResponseWrit
 		} else {
 			httprouter.NotFound(w, r)
 		}
+	}
+}
+
+func run(server *http.Server) {
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		panic(err)
+	}
+}
+
+func graceful(server *http.Server, timeout time.Duration) {
+	stop := make(chan os.Signal, 1)
+
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		panic(err)
 	}
 }
 
